cbaa75be2898531094769d666acc3528b699e21e
AKSHAYA.G <akshaya26.inn@gmail.com>
2026-02-05 21:01:48 +0530

    Add files via upload

---
+++ Digital Clock.java
@@ -0,0 +1,17 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class DigitalClock {
+    public static void main(String[] args) throws InterruptedException {
+
+        DateTimeFormatter format =
+                DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        while (true) {
+            LocalTime time = LocalTime.now();
+
+            System.out.print("\r Time: " + time.format(format));
+            Thread.sleep(1000);
+        }
+    }
+}
